1c532177c6d29d51c13a443bada790ef71cbb97e
Ilias <ilias@hamdaoui.com>
2023-02-15 09:57:08 +0100

    code cleanup

---
+++ SolenLmsApp/Api/Shared/Infrastructure/Events/AzureServiceBusEventsSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Imanys.SolenLms.Application.Shared.Infrastructure.Events;
+
+internal sealed class AzureServiceBusEventsSettings
+{
+    public string TopicName { get; set; } = default!;
+    public string SubscriptionName { get; set; } = default!;
+}
